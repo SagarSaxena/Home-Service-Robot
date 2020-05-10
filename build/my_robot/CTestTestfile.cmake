05289350814a291b3274ee24039e2b702eb438d8
root <sagar.saxena@alumni.stanford.edu>
2020-05-10 04:49:05 +0000

    Add .gitignore config

--- build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/Home-Service-Robot/src/my_robot
-# Build directory: /home/workspace/Home-Service-Robot/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/turtlebot/turtlebot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/Home-Service-Robot/src/turtlebot/turtlebot
-# Build directory: /home/workspace/Home-Service-Robot/build/turtlebot/turtlebot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
