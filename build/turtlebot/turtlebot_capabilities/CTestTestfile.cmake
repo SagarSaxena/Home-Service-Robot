98f6f1aab01c797070e9eb41f5591fc86a8d529c
root <sagar.saxena@alumni.stanford.edu>
2020-05-10 04:39:37 +0000

    Migrate final project to clean repo

---
+++ build/turtlebot/turtlebot_capabilities/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/Home-Service-Robot/src/turtlebot/turtlebot_capabilities
+# Build directory: /home/workspace/Home-Service-Robot/build/turtlebot/turtlebot_capabilities
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/turtlebot_interactions/turtlebot_rviz_launchers/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/Home-Service-Robot/src/turtlebot_interactions/turtlebot_rviz_launchers
+# Build directory: /home/workspace/Home-Service-Robot/build/turtlebot_interactions/turtlebot_rviz_launchers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
